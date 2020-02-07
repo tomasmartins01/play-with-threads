e150f393e122d88a611b2abe706f17fa7fe78ee1
Tiago Reis <reis.tiago@gmail.com>
2020-02-07 12:58:14 +0000

    Initial commit

---
+++ src/main/java/com/mindera/school/FibonacciCalculator.java
@@ -0,0 +1,31 @@
+package com.mindera.school;
+
+import java.util.List;
+
+/**
+ * Used to calculate the Fibonacci sequence
+ *
+ * https://en.wikipedia.org/wiki/Fibonacci_number
+ */
+public class FibonacciCalculator {
+
+    /**
+     * Calculates n entries of the fibonacci sequence
+     * <p>
+     * In mathematics, the Fibonacci numbers, commonly denoted Fn, form a sequence, called the Fibonacci sequence,
+     * such that each number is the sum of the two preceding ones, starting from 0 and 1
+     * <p>
+     * eg: 0,1,1,2,3,5,8,13,21,34,55,89,144
+     */
+    public String fibonacciSequence(final Integer n) {
+        return "";
+    }
+
+    /**
+     * Calculates n entries of the fibonacci sequence for each entry in the list
+     */
+    public List<String> sequences(List<Integer> entries) {
+        // TODO you should use threads for this
+        return List.of("");
+    }
+}
---
+++ src/test/java/com/mindera/school/FibonacciCalculatorTest.java
@@ -0,0 +1,33 @@
+package com.mindera.school;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciCalculatorTest {
+
+    private FibonacciCalculator fibonacciCalculator;
+
+    @BeforeEach
+    void setup() {
+        this.fibonacciCalculator = new FibonacciCalculator();
+    }
+
+    @Test
+    void highestFibonacci() {
+        assertEquals("0,1,1,2,3", fibonacciCalculator.fibonacciSequence(5));
+    }
+
+    @Test
+    void fibonacciSequence() {
+        var results = fibonacciCalculator.sequences(List.of(5, 6, 7));
+        assertEquals("0,1,1,2,3", results.get(0));
+        assertEquals("0,1,1,2,3,5", results.get(1));
+        assertEquals("0,1,1,2,3,5,8", results.get(2));
+    }
+
+    // TODO - Can you write a test that doesn't guarantee order but is potentially faster?
+}
